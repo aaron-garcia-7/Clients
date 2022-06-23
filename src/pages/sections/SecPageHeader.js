aec739d3c164e7e1fba3d8df471ae2603b63b06c
Aaron Garcia <97797254+aaron-garcia-7@users.noreply.github.com>
2022-06-23 14:23:59 -0600

    Delete src directory

--- src/pages/sections/SecPageHeader.js
+++
@@ -1,9 +0,0 @@
-const SecPageHeader = () => {
-  return (
-    <section>
-      <h1>About Page</h1>
-    </section>
-  );
-};
-
-export default SecPageHeader;
--- src/pages/subPages/TestimonialsPage.js
+++
@@ -1,9 +0,0 @@
-const TestimonialsPage = () => {
-  return (
-    <section>
-      <h1>Testimonials Page</h1>
-    </section>
-  );
-};
-
-export default TestimonialsPage;
