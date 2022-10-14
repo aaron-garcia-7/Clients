fc2d51572b9bca8c214114f34687ae68555ae037
Aaron Garcia <97797254+aaron-garcia-7@users.noreply.github.com>
2022-10-14 12:43:05 -0600

    Add files via upload
    Updated the "Teams" and "Our Work" pages. Added a mobile navigation menu which led me to altering the version of 'react-router-dom' to allow use of the '.active' class, which wasn't working in the most up to date version. Reverted back to rrd version 6.3.0. Also adjusted cubic-beziers for hero text (to prevent fall back) as well as the timing function for ScrollToTop.

---
+++ iconicCreative/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import GlobalStyle from "./GlobalStyles";
+
+// Pages
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import OurWork from "./pages/OurWork";
+import Team from "./pages/Team";
+
+// Components
+import Nav from "./components/Nav";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+import ScrollToTop from "./router/ScrollToTop";
+
+function App() {
+  const [navOpen, setNavOpen] = useState(false);
+  const [pageWidth, setPageWidth] = useState(window.innerWidth);
+  const [fromTop, setFromTop] = useState(false);
+  const [y, setY] = useState(window.scrollY);
+
+  const listenFromTop = () => {
+    window.scrollY > 200 ? setFromTop(true) : setFromTop(false);
+  };
+
+  useEffect(() => {
+    setY(window.scrollY);
+    window.addEventListener("scroll", listenFromTop);
+
+    return () => {
+      window.removeEventListener("scroll", listenFromTop);
+    };
+  }, [fromTop]);
+
+  const checkWidth = () => {
+    setPageWidth(window.innerWidth);
+  };
+
+  const closeMenuResize = () => {
+    return window.innerWidth > 768 ? setNavOpen(false) : null;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkWidth);
+    window.addEventListener("resize", closeMenuResize);
+
+    return () => {
+      window.removeEventListener("scroll", checkWidth);
+      window.removeEventListener("resize", closeMenuResize);
+    };
+  }, [pageWidth]);
+
+  const location = useLocation();
+
+  return (
+    <div>
+      <GlobalStyle fromTop={fromTop} />
+      <Nav navOpen={navOpen} setNavOpen={setNavOpen} />
+      <Menu navOpen={navOpen} setNavOpen={setNavOpen} />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home pageWidth={pageWidth} />} />
+          <Route
+            path="/Services"
+            element={<Services pageWidth={pageWidth} />}
+          />
+          <Route path="/OurWork" element={<OurWork />} />
+          <Route path="/Team" element={<Team />} />
+        </Routes>
+      </AnimatePresence>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ iconicCreative/src/pages/sections/About.js
@@ -0,0 +1,152 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import aboutBg from "../../images/photos/aboutBg.png";
+
+function About() {
+  const [ref, inView] = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
+
+  return (
+    <ScAbout ref={ref}>
+      <img src={aboutBg} alt="" className="aboutBg" />
+      <article className="aboutContent">
+        {inView && <h2 className="aboutTitle">| About</h2>}
+        {inView && (
+          <p className="aboutText">
+            Iconic Creative is a branding, web design, and social media agency
+            located in Utah. We power some of the most aspiring and
+            design-forward brands.
+          </p>
+        )}
+      </article>
+      <button className="aboutBtn">
+        <NavLink to="/Team">Meet The Team</NavLink>
+        <FontAwesomeIcon icon={faChevronRight} className="faIcon" />
+      </button>
+    </ScAbout>
+  );
+}
+
+const ScAbout = styled("section")`
+  width: 100vw;
+  height: 100vh;
+  min-height: 36rem;
+
+  > * {
+    position: absolute;
+  }
+
+  .aboutBg {
+    top: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .aboutContent {
+    /* border: 1px solid red; */
+    top: 50%;
+    left: 6%;
+    transform: translate(0, -50%);
+    width: 80%;
+    .aboutTitle {
+      font-size: calc(1.2rem + 6vw);
+      margin-bottom: 4vw;
+      opacity: 0;
+      transform: skew(-16deg, 1.2deg);
+      animation: skewIn 0.6s ease 0.2s forwards;
+    }
+    .aboutText {
+      font-size: calc(0.6rem + 3.2vw);
+      opacity: 0;
+      transform: skew(-12deg, 0.2deg);
+      animation: skewIn 0.8s ease 0.3s forwards;
+    }
+  }
+
+  .aboutBtn {
+    position: relative;
+    top: 86%;
+    left: 64%;
+    transform: scale(1.2);
+    z-index: 2;
+    a {
+      color: var(--dark);
+      font-size: calc(0.8rem + 0.8vw);
+      font-weight: 400;
+      transition: color 0.4s ease 0.2s;
+    }
+    .faIcon {
+      position: absolute;
+      top: 50%;
+      right: -6%;
+      transform: translate(100%, -50%);
+      font-size: calc(0.6rem + 0.6vw);
+      transition: transform 0.4s cubic-bezier(0.54, -0.85, 0.45, 1.67);
+    }
+    &::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: -12%;
+      transform: translate(0%, -50%);
+      width: 4rem;
+      height: 4rem;
+      border-radius: 12rem;
+      background: var(--grey);
+      z-index: -1;
+      pointer-events: none;
+      transition: width 0.4s cubic-bezier(0.42, -0.49, 0.64, 1.24),
+        background 0.4s ease;
+    }
+    &:hover {
+      a {
+        color: var(--light);
+      }
+      .faIcon {
+        /* font-size: calc(0rem + 0vw); */
+        transform: translate(250%, -50%);
+        transition-delay: 0.16s;
+      }
+      &::before {
+        width: 124%;
+        background: var(--offDark);
+      }
+    }
+  }
+
+  @media (max-width: 1440px) {
+    .aboutBtn {
+      transform: scale(1);
+    }
+  }
+
+  @media (max-width: 768px) {
+    height: 68vh;
+    min-height: 32rem;
+    .aboutBtn {
+      left: 64%;
+      &::before {
+        width: 3.2rem;
+        height: 3.2rem;
+      }
+    }
+  }
+
+  @media (max-width: 480px) {
+    height: 54vh;
+    min-height: 24rem;
+    .aboutBtn {
+      top: 84%;
+      left: 50%;
+    }
+  }
+`;
+
+export default About;
