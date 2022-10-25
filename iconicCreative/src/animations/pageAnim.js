c55f5f134355e2c075ca4b55abe8b1f3c3822627
Aaron Garcia <97797254+aaron-garcia-7@users.noreply.github.com>
2022-10-25 09:41:49 -0600

    Add files via upload
    Added the pageBanner effect that Daniel asked for, which came with a few needed alterations to the navigation. I also updated the footer social media icons to be consistent with one another.

---
+++ iconicCreative/src/animations/pageAnim.js
@@ -0,0 +1,20 @@
+export const pageAnim = {
+  initial: {
+    opacity: 0,
+    y: 40,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: 40,
+    transition: {
+      duration: 0.6,
+    },
+  },
+};
---
+++ iconicCreative/src/data/brandsData.js
@@ -0,0 +1,57 @@
+import brand1 from "../images/graphics/brand1.svg";
+import brand2 from "../images/graphics/brand2.svg";
+import brand3 from "../images/graphics/brand3.svg";
+import brand4 from "../images/graphics/brand4.svg";
+import brand5 from "../images/graphics/brand5.svg";
+import brand6 from "../images/graphics/brand6.svg";
+import brand7 from "../images/graphics/brand7.svg";
+
+const brandData = [
+  {
+    name: "",
+    img: null,
+  },
+  {
+    name: "Briizy",
+    img: brand1,
+    img2: null,
+  },
+  {
+    name: "Vitality",
+    img: brand2,
+    img2: null,
+  },
+  {
+    name: "Sundek",
+    img: brand3,
+    img2: null,
+  },
+  {
+    name: "Enzy",
+    img: brand4,
+    img2: null,
+  },
+  {
+    name: "Stamina",
+    img: brand5,
+    img2: null,
+  },
+  {
+    name: "Affinity",
+    img: brand6,
+    img2: null,
+  },
+  {
+    name: "Bak'd",
+    img: brand7,
+    img2: null,
+  },
+  {
+    name: "",
+    img: null,
+  },
+];
+
+brandData.forEach((obj, index) => (obj.id = index + 1));
+
+export default brandData;
